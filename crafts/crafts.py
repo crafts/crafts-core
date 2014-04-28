09e3034e502dc5c88c1d7ce7c6cd617b7d56f4fb
Andrew Guenther <guenther.andrew.j@gmail.com>
2014-04-28 01:04:53 -0700

    Added a simple script for setting up the CouchDB
    Script can create a databse in CouchDB for crafts and also insert an
    initial configuration and add attachments to it. It can also delete this
    database. This script will be used later to insert all of the views. It
    is also the first piece of code to use the new CouchDB driver.

---
+++ crafts/crafts.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+'''
+Crafts
+
+Usage:
+        crafts init <config-file> [<attachments>...] [options]
+        crafts clear [options]
+        crafts -h
+
+Options:
+        -h --help       Show this screen.
+        --version       Show version.
+        -H url          CouchDB url [default: http://localhost:5984/]
+        -D db           CouchDB database [default: crafts]
+'''
+from docopt import docopt
+from couchdb.client import Server
+import json
+
+if __name__ == '__main__':
+    args = docopt(__doc__, version='Crafts 0.1')
+
+    couch = Server(args['-H'])
+
+    if args['init']:
+        db = couch.create(args['-D'])
+        with open(args['<config-file>']) as config_file:
+            config = json.load(config_file)
+            config['_id'] = 'config'
+            (doc_id, rev_id) = db.save(config)
+            for attachment_file in args['<attachments>']:
+                with open(attachment_file) as attachment:
+                    db.put_attachment({'_id':doc_id, '_rev':rev_id}, attachment)
+    elif args['clear']:
+        del couch[args['-D']]
