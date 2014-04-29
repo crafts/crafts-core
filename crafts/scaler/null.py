6512419eeaf6fa2db79edf814b368df86bf00c31
Andrew Guenther <guenther.andrew.j@gmail.com>
2014-04-29 02:04:17 -0700

    Replaced the ARTS scaler to NullScaler.
    Since ARTS only generates request-based workloads, the scaler component
    is irrelevant for ARTS simulations. A more general "Null" scaler may
    also be useful in other situations, like simply visualizing scaling
    plans rather than implementing them.

---
+++ crafts/scaler/null.py
@@ -0,0 +1,16 @@
+import crafts.scaler
+import uuid
+
+class NullScaler(crafts.scaler.ScalerDriver):
+    def __init__(self, cluster):
+        self.cluster = cluster
+
+    def scale_up(self, role, count):
+        for _ in xrange(count):
+            self.cluster[role].append(str(uuid.uuid4()))
+
+    def scale_down(self, role, count):
+        self.cluster[role] = self.cluster[role][:-count]
+
+    def describe_cluster(self):
+        return self.cluster
