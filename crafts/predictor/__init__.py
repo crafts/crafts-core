4355c88db7b7d4aedd5424667dd469823764c9eb
Andrew Guenther <guenther.andrew.j@gmail.com>
2014-04-29 06:10:35 -0700

    Added a really stupid predictor.
    Added a predictor which will look at the value for the given time the
    week before and use that as the prediction. This also reshaped a bit of
    how the prediction workflow will work.

--- crafts/predictor/__init__.py
+++ crafts/predictor/__init__.py
@@ -1,6 +1,22 @@
-class Predictor(object):
-   def __init__(self, config):
-      self.config = config
+from crafts.common import metrics
+from datetime import datetime
+from datetime import timedelta
 
-   def predict(self, start, stop):
-      raise NotImplementedError("predict needs to be implemented in subclass")
+class Predictor(object):
+    def predict(self, window, start_time, interval, cycle_size):
+        raise NotImplementedError("predict needs to be implemented in subclass")
+
+def make_prediction(db, predictor_cls, role, metric, window_size, cycle_start,
+        interval, cycle_size):
+    start = datetime.utcnow() - timedelta(days=window_size)
+    window = metrics.PredictionCollection(db)
+    window.get(role, metric, start)
+
+    predictor = predictor_cls()
+    return predictor.predict(window, cycle_start, interval, cycle_size)
+
+if __name__ == '__main__':
+    from couchdb import Server
+    from crafts.predictor.lame import LamePredictor
+
+    print make_prediction(Server()['crafts'], LamePredictor, 'arts', 'requests', 7, datetime.utcnow(), 1, 10)
--- crafts/predictor/lame.py
+++ crafts/predictor/lame.py
@@ -1,5 +1,15 @@
+from datetime import timedelta
 import crafts.predictor
 
 class LamePredictor(crafts.predictor.Predictor):
-   def predict(self, start, stop):
-      pass
+    def predict(self, window, start_time, interval, cycle_size):
+        predictions = []
+
+        for offset in xrange(0, cycle_size * interval, interval):
+            time = start_time + timedelta(minutes=offset)
+
+            result = window.find_nearest(time - timedelta(weeks=1))
+
+            predictions.append(result['avg'])
+
+        return predictions
