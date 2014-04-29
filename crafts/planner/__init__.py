d10c3674de1ed576ab2aa9a1a7ed51fb2cd92114
Andrew Guenther <guenther.andrew.j@gmail.com>
2014-04-29 06:36:48 -0700

    Dirty simple planner, does not hook into a scaler yet.
    Generic scheduling is going to be a challenge. That will come later.

--- crafts/planner/__init__.py
+++ crafts/planner/__init__.py
@@ -1,3 +1,11 @@
+from crafts.scaler.null import NullScaler
+import math
+
 class Planner(object):
    def __init__(self, config):
       self.config = config
+
+def build_plan(predictions, throughput, scaler=NullScaler()):
+    capacity = map(lambda p: math.ceil(p/throughput), predictions)
+
+    print capacity
