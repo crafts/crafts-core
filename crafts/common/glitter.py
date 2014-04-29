776abec74a5228842f52623ddc4f9aea0916c035
Andrew Guenther <guenther.andrew.j@gmail.com>
2014-04-29 16:48:19 -0700

    Migrated Glitter over to the couchdb library

--- crafts/common/glitter.py
+++ crafts/common/glitter.py
@@ -1,31 +1,16 @@
 import logging
 
 class GlitterHandler(logging.Handler):
-   def __init__(self, host='localhost', port=5984,
-         db='logs', user=None, password=None):
+   def __init__(self, url='http://localhost:5984/', db='logs'):
+      from couchdb import Server
 
-      auth = ''
-      if user is not None and password is not None:
-         auth = '{user}:{password}@'.format(user=user, password=password)
-
-      self.couch_url = 'http://{auth}{host}:{port}/{db}'.format(
-            auth=auth, host=host, port=port, db=db)
+      self.db = Server(url)[db]
 
       super(GlitterHandler, self).__init__()
 
    def emit(self, record):
       try:
-         import json
-         import requests
-
-         doc = record.__dict__
-         headers = {'content-type': 'application/json'}
-         res = requests.post(self.couch_url, json.dumps(doc), headers=headers);
-         if 'error' in res.json():
-            if res.json()['error'] == 'unauthorized':
-               raise IOError('Authentication Failed')
-            else:
-               raise IOError(res.json())
+         self.db.save(record.__dict__)
       except (KeyboardInterrupt, SystemExit):
          raise
       except:
