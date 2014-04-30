322f30e21dcdfec16691ca5467aecf6557ba2088
Andrew Guenther <guenther.andrew.j@gmail.com>
2014-04-29 17:15:49 -0700

    Arts now bypasses the MAL, this module is useless

--- crafts/mal/arts.py
+++
@@ -1,6 +0,0 @@
-class ARTSMALDriver(crafts.mal.MALDriver):
-   def __init__(self, generator=None):
-      self.generator = generator
-
-   def get_metrics(self, start, stop):
-      self.generator.get_metrics(start, stop)
