27b4cb07d302217e1912b9c72502df56c31366c4
Andrew Guenther <guenther.andrew.j@gmail.com>
2014-05-08 01:47:53 -0700

    FFT predictor now properly shifts up times

--- crafts/predictor/fft.py
+++ crafts/predictor/fft.py
@@ -10,6 +10,8 @@ class FFTPredictor(Predictor):
         print(freq_dom)
         freq_dom = map(lambda x: x if abs(x) > 500 else 0, freq_dom)
 
+        new_times = [(time - times[0]) + start_time for time in times]
+
         prediction = [round(x, 0) for x in ifft(freq_dom)]
 
-        return zip(times, prediction)
+        return zip(new_times, prediction)
