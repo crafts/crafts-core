3591e8517e8a0a72d7f78413ee0bad949b96c445
Andrew Guenther <guenther.andrew.j@gmail.com>
2014-06-07 22:05:13 -0700

    This script shouldn't be part of the repo

--- crafts/reload_wiki.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-~/thesis/crafts-core/crafts/crafts-cli.py clear -H http://andrew:password@localhost:5984/
-~/thesis/crafts-core/crafts/crafts-cli.py init config/config.json -H http://andrew:password@localhost:5984/
-~/thesis/arts/arts/arts.py load ~/thesis/data/wiki/output/part-0000* -H handlers.CraftsHandler
